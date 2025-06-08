6d61a5489fb9d79ba8c3fb9dbfc9b323ec16ad19
Connor <107011324+connor33341@users.noreply.github.com>
2025-06-08 19:20:07 -0400

    standard update
    Private key will be stored in /data/local/private.key, while the public will remain in NRI/RRI

--- src/lib/VoxaCommunications_Router/cryptography/keymanager.py
+++ src/lib/VoxaCommunications_Router/cryptography/keymanager.py
@@ -11,6 +11,7 @@ from lib.VoxaCommunications_Router.cryptography.keyutils import RSAKeyGenerator
 from lib.VoxaCommunications_Router.util.ri_utils import fetch_ri
 from lib.compression import JSONCompressor
 from util.jsonreader import read_json_from_namespace
+from util.filereader import file_to_str
 from util.logging import log
 
 class KeyManager:
@@ -18,6 +19,7 @@ class KeyManager:
         self.mode = mode
         self.logger = log()
         self.rsa_keys: dict = {}
+        self.cryptography_config = read_json_from_namespace("config.cryptography") or {}
 
     def load_rsa_keys(self) -> dict:
         storage_config = read_json_from_namespace("config.storage") or {}
@@ -27,3 +29,9 @@ class KeyManager:
         data_dir = storage_config.get("data-dir", "data/")
         file_name = "nri" if self.mode == "node" else "rri"  # if node then nri if relay then rri
         ri_data: dict = fetch_ri(file_name, path="local").get("data")
+        if not ri_data:
+            self.logger.error(f"No RI data found for mode: {self.mode}")
+            return {}
+        self.rsa_keys["public_key"] = ri_data.get("public_key")
+        self.rsa_keys["private_key"] = file_to_str(os.path.join(data_dir, "local", self.cryptography_config.get("private-key-file")))
+        
