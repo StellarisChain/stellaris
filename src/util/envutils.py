531a8f38e573f987f0da2f9570774ec828f42333
Connor <107011324+connor33341@users.noreply.github.com>
2025-06-15 17:14:19 -0400

    bug fix

--- src/util/envutils.py
+++ src/util/envutils.py
@@ -1,9 +1,9 @@
 import os
 
-def detect_container(self) -> bool:
+def detect_container() -> bool:
     """Detect if running in a container (Docker, etc.)."""
     # Check for common container indicators
-    container_indicators = [
+    container_indicators: list[bool] = [
         os.path.exists('/.dockerenv'),  # Docker
         os.environ.get('container') is not None,  # systemd-nspawn
         os.environ.get('KUBERNETES_SERVICE_HOST') is not None,  # Kubernetes
